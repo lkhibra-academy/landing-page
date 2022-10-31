626433589af6c8b28560a540900cbf6d2cb54410
El Mehdi Benennia <elmehdibenennia@gmail.com>
2022-10-31 15:06:33 +0000

    content fixed

--- src/partials/frontend/JoinUsTraining.jsx
+++ src/partials/frontend/JoinUsTraining.jsx
@@ -12,7 +12,7 @@ export default function JoinUsTraining() {
               <div className="flex flex-col w-full prose text-left max-w-max lg:max-w-2xl ">
                   <div dir='rtl' className="w-full mx-auto text-center">
                       <h1 className='h1 mb-4'>
-                      إنضم إلى أكثر من 40 طالب 
+                      إنضم إلى أكثر من 60 طالب 
                       </h1>
                       <p className='text-lg md:text-xl font-normal text-neutral-600'>
                       اختصر وقت تعلمك للمهارات الأكثر طلبا في سوق الشغل واحصل على شهادة معتمدة في التصميم والبرمجة
--- src/partials/home/JoinUsHome.jsx
+++ src/partials/home/JoinUsHome.jsx
@@ -7,7 +7,7 @@ export default function JoinUsHome() {
           <div className=" flex flex-col items-center max-w-6xl mx-auto px-5 sm:px-6 py-8 ">
               <div className="flex flex-col w-full prose text-left max-w-max lg:max-w-2xl ">
                   <div dir='rtl' className="w-full mx-auto text-center">
-                      <h1 className='h2 mb-4'>إنضم إلى أكثر من 40 طالب </h1>
+                      <h1 className='h2 mb-4'>إنضم إلى أكثر من 60 طالب </h1>
                       <p className='text-lg md:text-xl font-normal text-neutral-600'>
                       اختصر وقت تعلمك للمهارات الأكثر طلبا في سوق الشغل واحصل على شهادة معتمدة في التصميم والبرمجة
                       </p>
