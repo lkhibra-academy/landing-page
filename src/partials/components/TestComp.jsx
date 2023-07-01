4f2d90fd0b34de83fecb7d137a7abbd94a3a5131
El Mehdi Benennia <elmehdibenennia@gmail.com>
2023-07-01 11:57:18 +0000

    coupon size fixed

--- src/partials/components/TestComp.jsx
+++ src/partials/components/TestComp.jsx
@@ -4,7 +4,7 @@ export default function TestComp(props) {
   let content = {
     Arabic: {
       title1:" ؟ ",
-      title:"هل لديك كود خصم",
+      title:"هل لديك ",
       // title:"هل لديك كود خصم",
       item1:"رغبة في التعلم",
         item2:"حاسوب",
@@ -19,26 +19,24 @@ export default function TestComp(props) {
     : (content = content.French);
     return (
     <section className="relative max-w-6xl  
-    text-center px-7 mx-auto sm:px-10 md:pt-0">
+    text-center px-7 mx-auto sm:px-10 md:py-0 pb-4">
       <details className=" group">
                   <summary dir="rtl" className="flex items-center justify-center cursor-pointer 
                   pt-2 px-4 ">
-                    <p className="text-lg font-light text-gray-600 text-center">
+                    <p className="text-sm md:text-lg font-light md:font-medium text-gray-600 text-center">
                     {content.title}
-                    <span className="text-red-primary font-medium text-base"> COUPON CODE </span>
+                    <span className="text-red-primary font-medium text-sm md:text-base"> COUPON CODE </span>
                     {content.title1}
                     </p> 
                   </summary>
-                  <div class="my-3 mx-auto leading-relaxed text-gray-700 max-w-sm rounded-md flex flex-row justify-center items-center
-                  border-gray-800 outline-2 
+                  <div class="my-3 mx-auto leading-relaxed text-gray-700 max-w-sm rounded-md flex flex-row justify-center items-center border
+                  border-gray-200 shadow-lg
                   ">
-                    <input type="text"  placeholder=" " 
-                    class="shrink w-full rounded-l-md border-none outline-none
-                    shadow-md
+                    <input type="text"  placeholder="COUPON CODE" 
+                    class="shrink w-full rounded-l-md 
                     " />
                     <button type="button" class="text-white bg-red-primary px-3 py-2 
-                    rounded-r-md border-none outline-none
-                    shadow-md
+                    rounded-r-md
                     " >Apply</button>
                   </div>
 
