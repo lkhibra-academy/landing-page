80ad8a028328b58112babb575766e9924a559cd8
El Mehdi Benennia <elmehdibenennia@gmail.com>
2023-07-04 00:47:52 +0000

    thankyou design updated

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../partials/components/Header'
 import Footer from '../partials/components/Footer'
 import { useLocation } from 'react-router-dom'
+import ig from '../images/instagram.png';
+
 
 export default function ThankYou() {
     const loc=useLocation();
@@ -16,10 +18,10 @@ export default function ThankYou() {
         <main className="flex-grow">
         <section className='flex flex-col min-h-screen overflow-hidden mb-10'>
             <div className="relative mx-auto max-w-screen-2xl">
-                <div className='flex justify-center flex-col pt-10 bg-white md:py-10'>
-                <p className="font-bold md:text-2xl text-xl text-center pb-5 text-gray-800" dir="rtl">مرحبا بك في تدريب الخبرة فالبرمجة</p>
-                <div className="max-w-lg md:mx-auto mx-3 rounded-lg border border-gray-200 shadow-md
-                flex justify-center items-center flex-col ">
+                <div className='flex justify-center flex-col p-4'>
+                <p className="font-bold md:text-2xl text-xl text-center pb-5 text-gray-800 " dir="rtl">مرحبا بك في تدريب الخبرة فالبرمجة</p>
+                <div className="md:mx-auto p-2 md:p-4 rounded-lg border border-gray-200 shadow-md
+                flex justify-center items-center flex-col max-w-lg ">
                     {/* Head */}
                     <i className="fa-regular fa-check-circle text-6xl mt-2 p-2 text-center text-red-primary"></i>
                     <p className="p-2 font-semibold text-lg md:text-xl text-center text-red-primary">
@@ -29,6 +31,13 @@ export default function ThankYou() {
                     <p className="px-1 py-3 font-medium text-sm md:text-lg text-center text-gray-800">
                     جميع خطوات التسجيل تم إرسالها في بريدك الإلكتروني
 وسوف نتصل بك في أقرب وقت من أجل التأكيد </p>
+                    {/* <a href="https://www.instagram.com/lkhibra.ma/"
+                            className="btn text-white bg-red-primary
+                            hover:text-red-primary hover:outline hover:outline-1 hover:outline-red-primary hover:bg-white 
+                             mb-4 w-auto sm:mb-0"
+                    >
+                            بغيت نتعلم
+                    </a> */}
 
                 </div>
                 <p className="font-light text-xs text-center p-2" dir="rtl">
@@ -42,6 +51,16 @@ export default function ThankYou() {
         </main>
         {/*  Site footer */}
         <Footer />
+        <a href="https://www.instagram.com/_u/lkhibra.ma" 
+        className="whatsapp-button z-50 sm:hidden" 
+            target="_blank" style={{
+                position: 'fixed',
+                right: '10px',
+                bottom: '20px'
+            }} 
+            >
+                <img src={ig} height={60} width={60}/>
+        </a>
 
 </div>
     )
