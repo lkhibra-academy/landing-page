baf0152b086bed753e337096d59d0bff73e11f9f
El Mehdi Benennia <elmehdibenennia@gmail.com>
2023-12-03 16:20:17 +0100

    FR.json with RTL for DoubleSec fixed

--- _components/ForWho.jsx
+++ _components/ForWho.jsx
@@ -8,7 +8,7 @@ exports.default = function ForWho({ ctx, name }) {
       <div class="py-8 md:py-8">
         <div class="md:gap-4">
           <div class="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6">
-            <p class="px-2 mb-6 font-bold leading-tight text-2xl md:text-3xl text-right md:text-center">
+            <p class="px-2 mb-6 font-bold leading-tight text-2xl md:text-3xl text-right ltr:text-left md:text-center">
               {i18n(name + ".title")}
             </p>
             <div class=" ">
